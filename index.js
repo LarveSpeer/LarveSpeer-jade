9a217dff85208427a54b3d87eddf368e415e2e1e
Jannik Lorenz <git@janniklorenz.de>
2015-06-07 00:34:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+/* @flow */
+
+var fs = require("fs")
+var path = require("path")
+var jade = require("jade")
+var express = require("express")
+var marked = require('marked')
+
+module.exports = function(pathToWorkingDir){
+	var app = express()
+	app.locals.path = pathToWorkingDir
+	app.locals.config = require(path.resolve(app.locals.path, "config.js"))
+
+
+	// this function provides the HTML code, which one will be displayed to the page
+	app.html = function() {
+		var layoutPath = path.resolve(app.locals.path, app.locals.config.layout)
+		return jade.compileFile(layoutPath)
+	}
+
+	// here we can return LESS css, which will only effect the page HTML code
+	app.less = function(){
+		var lessPath = path.resolve(app.locals.path, app.locals.config.less)
+		return fs.readFileSync(lessPath).toString()
+	}
+
+
+	return app
+}
